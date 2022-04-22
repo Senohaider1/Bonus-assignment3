7b9f925b4f18e7e82e3c8f5b01fc2778cb74226b
Senohaider1 <101878220+Senohaider1@users.noreply.github.com>
2022-04-22 02:52:33 +0200

    filters 3,6,9,c

--- main cpp.cpp
+++ main cpp.cpp
@@ -11,6 +11,10 @@ unsigned char image2[SIZE][SIZE][RGB];
 void loadImage ();
 void saveImage ();
 void blackandwhite ();
+void mergeimage ();
+void darkenandlightenimage ();
+void shrinkimage ();
+void blurimage();
 
 int main()
 {
@@ -100,4 +104,141 @@ void blackandwhite() {
             }
         }
     }
+}
+
+void mergeimage (){
+    for (int i=0; i<SIZE; i++) {
+        for (int j=0; j<SIZE; j++) {
+                for(int g= 0 ;g<RGB;g++){
+            image2[i][j][g] = image[i][j][g];}
+        }
+    }
+    cout <<"Enter the file name of the second photo\n";
+    loadImage();
+    for (int i=0; i<SIZE; i++) {
+        for (int j=0; j<SIZE; j++) {
+            for(int g = 0 ;g<RGB;g++){
+            image[i][j][g] = (image2[i][j][g]+image[i][j][g])/2;}
+        }
+    }
+}
+void darkenandlightenimage (){
+    char choose6;
+    cout<<"Do you want to (d)arken or (l)ighten :";
+    cin>>choose6;
+    if(choose6=='d'){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j< SIZE; j++){
+                for (int c=0;c<3;++c)
+                    image[i][j][c] /=2;
+            }
+        }
+    }
+    else if(choose6=='l'){
+        int num=0;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j< SIZE; j++){
+                for (int c=0;c<3;++c){
+                    num=image[i][j][c]*2;
+                    if (num>255)    num=255;
+                    image[i][j][c]=num;
+                }
+            }
+        }
+    }
+    else cout<<"ERROR INVALID CHOOSE";
+}
+
+void shrinkimage (){
+    float choose9;
+    cout<<"1-Shrink to (1/2)\n2-Shrink to (1/3) \n3-Shrink to (1/4) \nchoose :";
+    cin>>choose9;
+    if (choose9==1){
+        for (int i = 0; i < SIZE; i++){
+            for (int j =0 ; j <SIZE; j++){
+                for (int c=0 ; c<RGB ; c++){
+                image[i/2][j/2][c]=image[i][j][c];
+                }
+            }
+        }
+        for (int i = 0; i < SIZE; i++){
+            for (int j =SIZE/2 ; j <SIZE; j++){
+                    for (int c=RGB/255 ; c<RGB ; c++){
+                image[i][j][c]=255;
+                    }
+            }
+        }
+        for (int i = SIZE/2; i < SIZE; i++){
+            for (int j =0 ; j <SIZE; j++){
+                    for(int c=RGB/255 ; c<RGB ; c++){
+                image[i][j][c]=255;
+                    }
+            }
+        }
+    }
+    if (choose9==2){
+        for (int i = 0; i < SIZE; i++){
+            for (int j =0 ; j <SIZE; j++){
+                for (int c=0 ; c<RGB ; c++){
+                image[i/3][j/3][c]=image[i][j][c];
+                }
+            }
+        }
+        for (int i = 0; i < SIZE; i++){
+            for (int j =SIZE/3 ; j <SIZE; j++){
+                    for (int c=RGB/255 ; c<RGB ; c++){
+                image[i][j][c]=255;
+                    }
+            }
+        }
+        for (int i = SIZE/3; i < SIZE; i++){
+            for (int j =0 ; j <SIZE; j++){
+                    for(int c=RGB/255 ; c<RGB ; c++){
+                image[i][j][c]=255;
+                    }
+            }
+        }
+    }
+    if (choose9==3){
+        for (int i = 0; i < SIZE; i++){
+            for (int j =0 ; j <SIZE; j++){
+                for (int c=0 ; c<RGB ; c++){
+                image[i/4][j/4][c]=image[i][j][c];
+                }
+            }
+        }
+        for (int i = 0; i < SIZE; i++){
+            for (int j =SIZE/4 ; j <SIZE; j++){
+                    for (int c=RGB/255 ; c<RGB ; c++){
+                image[i][j][c]=255;
+                    }
+            }
+        }
+        for (int i = SIZE/4; i < SIZE; i++){
+            for (int j =0 ; j <SIZE; j++){
+                    for(int c=RGB/255 ; c<RGB ; c++){
+                image[i][j][c]=255;
+                    }
+            }
+        }
+    }
+}
+
+void blurimage()
+{
+    char choosec;
+    for(int p = 0; p < 7; p++)
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j =0 ; j <SIZE; j++)
+            {
+                 for (int c =0 ; c <RGB; c++)
+                 {
+                     image[i][j][c]= (image[i+1][j][c]+ image[i-1][j][c]+ image[i][j+1][c]+ image[i][j-1][c]) /4; 
+            }
+        } 
+
+    }
+  }
 }
